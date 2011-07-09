7362d10192745da2bbaf0e24ae096e55bfa0d339
Matzo <ksk.matsuo@gmail.com>
2011-07-10 00:04:33 +0900

    Add Copyright

--- MZNavigationControllerModule/MZNavigationScrollView.m
+++ MZNavigationControllerModule/MZNavigationScrollView.m
@@ -3,7 +3,7 @@
 //  MZNavigationScrollView
 //
 //  Created by Keisuke Matsuo on 11/05/16.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Keisuke Matsuo. All rights reserved.
 //
 
 #import "MZNavigationScrollView.h"
